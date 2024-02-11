0faea571f4dde8e017ae6a624e532f8932dafd73
Sayali Shirke <sayalishirke27@gmail.com>
2024-02-11 22:48:14 +0530

    scripts added

--- 01-node-tutorial/1-event-loop-examples/1-read-file.js
+++ 01-node-tutorial/1-event-loop-examples/1-read-file.js
@@ -2,6 +2,8 @@ const { readFile, writeFile } = require('fs')
 
 console.log('started a first task')
 // CHECK FILE PATH!!!!
+
+// callbacks get offloaded
 readFile('./content/first.txt', 'utf8', (err, result) => {
   if (err) {
     console.log(err)
